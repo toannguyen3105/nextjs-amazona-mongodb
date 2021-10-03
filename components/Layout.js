59ad7594e3fe52d2b0071213cca55071e87a79e6
tony <nguyenhuytoan1994@outlook.com>
2021-10-03 16:32:55 +0700

    first commit

---
+++ components/Layout.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Head from "next/head";
+import NextLink from "next/link";
+import {
+  AppBar,
+  Container,
+  Link,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import useStyles from "../utils/styles";
+
+export default function Layout({ children }) {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Head>
+        <title>Next Amazona</title>
+      </Head>
+
+      <AppBar position="static" className={classes.navbar}>
+        <Toolbar>
+          <NextLink href="/" passHref>
+            <Link>
+              <Typography className={classes.brand}>amazona</Typography>
+            </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <div>
+            <NextLink href="/cart" passHref>
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href="/login" passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
+        <Typography>All rights reserved. Next Amazona</Typography>
+      </footer>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,49 @@
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+import Layout from "../components/Layout";
+import data from "../utils/data";
+import NextLink from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <Layout>
+        <h1>Products</h1>
+        <Grid container spacing={3}>
+          {data.products.map((product) => (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <NextLink href={`/product/${product.slug}`} passHref>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      image={product.image}
+                      title={product.name}
+                    ></CardMedia>
+                    <CardContent>
+                      <Typography>{product.name}</Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </NextLink>
+                <CardActions>
+                  <Typography variant="h6">${product.price}</Typography>
+                  <Button size="small" color="primary">
+                    Add to cart
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Layout>
+    </>
+  );
+}
